c4ee11fb590ffb45147421eda91a081972072ce2
Vamsi4222 <119730041+Vamsi4222@users.noreply.github.com>
2023-08-25 09:57:30 +0530

    import java.util.*; public class vamsi{     public static void main(String[]args){         Scanner n = new Scanner (System.in);         String vamsi=n.nextLine();         System.out.println(vamsi);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class vamsi{
+    public static void main(String[]args){
+        Scanner n = new Scanner (System.in);
+        String vamsi=n.nextLine();
+        System.out.println(vamsi);
+    }
+}
