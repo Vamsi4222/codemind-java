65525b88dfade606043b7101fc099387f5e3d75d
Vamsi4222 <119730041+Vamsi4222@users.noreply.github.com>
2023-08-25 10:05:41 +0530

    import java.util.*; public class t{     public static void main(String[]args){         float a,b;         Scanner n=new Scanner(System.in);         a=n.nextFloat();         b=n.nextFloat();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class t{
+    public static void main(String[]args){
+        float a,b;
+        Scanner n=new Scanner(System.in);
+        a=n.nextFloat();
+        b=n.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
